1b389b7213dd0d8150845cbabbb7e5c3f542c383
julietamponti <jponti@fi.uba.ar>
2024-03-22 22:27:47 -0300

    Add search attraction by cordinates

--- app/routers/attractions/attractions_router.py
+++ app/routers/attractions/attractions_router.py
@@ -18,8 +18,8 @@ security = HTTPBearer()
 
 @router.get(
     "/attractions/byid/{attraction_id}",
-    status_code=201,
-    tags=["Get Attractions"],
+    status_code=200,
+    tags=["Attractions"],
     description="Gets an attraction given its ID",
     response_model=AttractionByID,
 )
@@ -51,8 +51,8 @@ def get_attraction(attraction_id: str, credentials: HTTPAuthorizationCredentials
 
 @router.post(
     "/attractions/search",
-    status_code=201,
-    tags=["Get Attractions"],
+    status_code=200,
+    tags=["Attractions"],
     description="Searches attractions given a text query",
 )
 def search_attraction_by_text(attraction: str, credentials: HTTPAuthorizationCredentials = Depends(security)):
@@ -77,3 +77,29 @@ def search_attraction_by_text(attraction: str, credentials: HTTPAuthorizationCre
         raise e
     except APIException as e:
         raise APIExceptionToHTTP().convert(e)
+
+# Search attractions by coordinates
+
+@router.post(
+    "/attractions/nearby/{latitude}/{longitude}/{radius}",
+    status_code=201,
+    tags=["Get Attractions"],
+    description="Gets nearby attractions given a latitude and longitude",
+)
+def get_nearby_attractions(latitude: float, longitude:float, radius: float, credentials: HTTPAuthorizationCredentials = Depends(security)):
+    try:
+        if check_authentication(credentials):
+            response: Response = requests.post(
+                f"http://attractions:8003/attractions/nearby/{latitude}/{longitude}/{radius}",
+            )
+
+            if response.status_code != 201:
+                raise HTTPException(status_code=response.status_code, detail=response.json()["detail"])
+
+            attractions_info = response.json()
+            return  attractions_info
+    except HTTPException as e:
+        raise e
+    except APIException as e:
+        raise APIExceptionToHTTP().convert(e)
+
