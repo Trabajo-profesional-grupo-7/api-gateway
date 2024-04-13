6f92c005045227b0cd6c44ad1f485f1ffe05856d
julietamponti <jponti@fi.uba.ar>
2024-04-13 12:05:15 -0300

    Refactor parsing day in weather endpoint

--- app/services/external_services/weather_services.py
+++ app/services/external_services/weather_services.py
@@ -13,7 +13,16 @@ def parse_weather_days(weather_data: dict):
     for item in weather_data.get("five_day_weather", []):
         date_str = item.get("date")
         weather_info = item.get("weather", {})
-        weather = Weather(**weather_info)
+        weather = Weather(
+            humidity=int(weather_info.get("humidity", 0)),
+            cloudCover=int(weather_info.get("cloudCover", 0)),
+            precipitation_probability=int(
+                weather_info.get("precipitation_probability", 0)
+            ),
+            temperature=weather_info.get("temperatureAvg", 0),
+            uv_index=weather_info.get("uv_index", 0),
+            visibility=weather_info.get("visibility", 0),
+        )
         day_weather = DayWeather(date=datetime.fromisoformat(date_str), weather=weather)
         five_day_weather.append(day_weather)
 
