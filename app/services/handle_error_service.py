1a9c70271852fa5a5baa5e4d18ae434ad8777096
Matías Rotondo <matiasnicolasrotondo@gmail.com>
2024-04-17 13:33:45 -0300

    Fix get metadata

--- app/services/handle_error_service.py
+++ app/services/handle_error_service.py
@@ -3,6 +3,4 @@ from fastapi import HTTPException
 
 def handle_response_error(current_status_code, response):
     if response.status_code != current_status_code:
-        raise HTTPException(
-            status_code=response.status_code, detail=response.json()["detail"]
-        )
+        raise HTTPException(status_code=response.status_code, detail=response.json())
